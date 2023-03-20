e84e718fec926f50b198b76fd64ca7a203e48589
Muhammad Kashif Bilal <kashif12702@gmail.com>
2023-03-20 18:42:43 +0500

    Simple landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,26 @@ import { useAuth } from "util/auth";
 import Link from "next/link";
 function IndexPage(props) {
   const auth = useAuth();
-  console.log(auth.user)
+  console.log(auth.user);
   return (
     <>
       <Meta />
       <section className="py-12 px-4">
-        <div className="container mx-auto">
-          This is a placeholder template, blah, blah.
-        </div>
         <div>
+          <div className="flex flex-col justify-center items-center">
+            <img
+              className=" w-[130px] block"
+              src="/Images/logo.png"
+              alt="Logo_GPCJ"
+            />
+            <h1 className="text-5xl text-center font-bold mt-6">
+              Government Post Graduate College Jhang
+            </h1>
+          </div>
           <Link href="/dashboard">
-          getstarted
+            <button className="flex mt-6 mx-auto rounded-md bg-[#a02d29] hover:bg-[#a74d4a] py-2 px-3 text-lg font-semibold text-white shadow-sm ">
+              Get Started
+            </button>
           </Link>
         </div>
       </section>
